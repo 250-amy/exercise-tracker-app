a41f6fa617d765961e4c8e998b44a48601fb7c5b
250-amy <amy.tencer@gmail.com>
2022-10-15 12:12:57 -0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <div class="w-40 h-40 bg-pink-500">
+  </div>
+</template>
+
